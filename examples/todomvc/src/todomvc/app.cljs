d3a4883f522c2ef6d33cbfb5ccc4e7b625d78b91
Dan Holmsand <holmsand@gmail.com>
2015-09-07 16:58:17 +0200

    Add local-storage support

--- examples/todomvc/src/todomvc/app.cljs
+++ examples/todomvc/src/todomvc/app.cljs
@@ -1,31 +1,55 @@
 (ns todomvc.app
   (:require [reagent.core :as r]
-            [todomvc.core :as core :refer [dispatch]]
+            [todomvc.core :as core]
             [secretary.core :as secretary :refer-macros [defroute]]
             [goog.events :as events]
             [goog.history.EventType :as etype])
   (:import goog.History))
 
-(defroute "/" [] (dispatch [:show :all]))
-(defroute show-path "/:show" [show] (dispatch [:show (keyword show)]))
+
+;;; Navigation handling
+
+(defroute "/" [] (core/dispatch [:show :all]))
+(defroute show-path "/:id" [id] (core/dispatch [:show (keyword id)]))
 
 (defonce history
   (doto (History.)
     (events/listen etype/NAVIGATE #(secretary/dispatch! (.-token %)))))
 
-(defn nav-handler [state [id val :as evt]]
+(defn nav-handler [state [id val]]
   (case id
-    :show (.setToken history (show-path {:show (name val)}))
+    :show (.setToken history (show-path {:id (name val)}))
     nil)
   state)
 
-(defn app-handler [state [id val :as event]]
+
+;;; Storage handling
+
+(def ls-key "reagent-todomvc-key")
+
+(defn read-local-storage [state]
+  (some->> (.getItem js/localStorage ls-key)
+           (cljs.reader/read-string)
+           (into (:todos state))
+           (assoc state :todos)))
+
+(defn write-local-storage [state]
+  (->> state :todos str
+       (.setItem js/localStorage ls-key))
+  state)
+
+
+;;; App
+
+(defn app-handler [state event]
   (-> state
+      (core/view-handler event)
       (nav-handler event)
-      (core/view-handler event)))
+      (write-local-storage)))
 
 (set! core/app-handler app-handler)
 
 (defn ^:export run []
+  (swap! core/todo-data read-local-storage)
   (.setEnabled history true)
   (r/render [core/todo-app] (js/document.getElementById "app")))
