7d9909675ecbed2d3417cff75a517acde5f131dd
Dan Holmsand <holmsand@gmail.com>
2016-06-16 18:14:09 +0200

    Update figwheel

--- project.clj
+++ project.clj
@@ -21,8 +21,8 @@
                                        :compiler
                                        {:main "reagenttest.runtests"}}}}}
 
-             :fig [{:dependencies [[figwheel "0.5.4-2"]]
-                    :plugins [[lein-figwheel "0.5.4-2"]]
+             :fig [{:dependencies [[figwheel "0.5.4-3"]]
+                    :plugins [[lein-figwheel "0.5.4-3"]]
                     :source-paths ["demo"] ;; for lighttable
                     :resource-paths ["site" "outsite"]
                     :figwheel {:css-dirs ["site/public/css"]}
