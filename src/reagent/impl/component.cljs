aebe9cc8e0260efab843c8ab42612b06b765c929
Dan Holmsand <holmsand@gmail.com>
2016-06-20 15:53:00 +0200

    Fix call to run-reactive

--- src/reagent/impl/component.cljs
+++ src/reagent/impl/component.cljs
@@ -139,7 +139,7 @@
                     (if (nil? rat)
                       (ratom/run-in-reaction #(do-render c) c "cljsRatom"
                                              batch/queue-render rat-opts)
-                      (._run-reactive rat false))))))})
+                      (._run-reactive rat))))))})
 
 (defn custom-wrapper [key f]
   (case key
